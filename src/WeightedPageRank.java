9f8f4731a60cc0537fa5fb8b8e05135bff6d47d2
Chenyang Tang <ct1856@nyu.edu>
2019-01-03 18:30:26 -0500

    init

---
+++ src/WeightedPageRank.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Chenyang Tang
+ *
+ */
+public class WeightedPageRank {
+
+  /**
+   * 
+   */
+  public WeightedPageRank() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
